5600be80b705e9b0acbcf17373993fb5a2f84e1f
Luiz Sanches <lsanches@protonmail.com>
2021-09-22 15:18:11 -0300

    Calendar display improvements

--- app/helpers/decidim/event_calendar/calendar_helper.rb
+++ app/helpers/decidim/event_calendar/calendar_helper.rb
@@ -21,7 +21,7 @@ module Decidim
         {
           title: translated_attribute(event.full_title),
           start: (event.start.strftime("%FT%R") unless event.start.nil?),
-          end: (event.finish.strftime("%FT%R") unless event.finish.nil?),
+          end: finish_date(event),
           color: event.color,
           url: event.link,
           resourceId: event.type,
@@ -36,8 +36,37 @@ module Decidim
           "name": "#{translated_attribute event.full_title} - #{translated_attribute event.subtitle}",
           "start": "#{event.start.strftime("%FT%R")}",
           "dependencies": "#{event.parent}",
-          "end": "#{event.finish.strftime("%FT%R")}"
+          "end": "#{event.finish.strftime("%FT%R")}",
+          "progress": #{progress_gantt(event)}
         })
+      end
+
+      private
+
+      def finish_date(event)
+        finish = event.finish
+
+        return if finish.nil?
+
+        finish = finish + 1.day if event.all_day?
+
+        finish.strftime("%FT%R")
+      end
+
+      def progress_gantt(event)
+        today = Date.today
+        start_date = event.start_date
+        end_date = event.end_date
+
+        return 0 if today < start_date
+
+        return 90 if today == end_date
+
+        return 100 if today > end_date
+
+        total_days = (end_date - start_date).to_i
+        days_performed = (today - start_date).to_i
+        (days_performed * 100) / total_days
       end
     end
   end
